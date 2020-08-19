5ebc51cfe4dc630cd3960ecbec0bf3a4b54e09dc
yash001dev <y.sanghavi001@gmail.com>
2020-08-19 17:14:22 +0530

    Adding Header Component and Links to REST Application.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -42,7 +42,7 @@ class Directory extends React.Component{
             <div className="directory-menu">
                 {
                     this.state.sections.map(({id,...otherSectionProps})=>(
-                        <MenuItem key={id} otherSectionProps  />
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
