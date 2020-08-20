f15d14fb28c695145af74399fffbd5198376bf90
yash001dev <y.sanghavi001@gmail.com>
2020-08-20 14:57:15 +0530

    SignIn Frontend Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage}/>
+      <Route path='/signin' component={SignInAndSignUpPage} />
     </Switch>
     </div>
   );
