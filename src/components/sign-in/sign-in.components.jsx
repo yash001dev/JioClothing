6f0e0907de849b28d15f8a8ca97918e2c1e80ce8
yash001dev <y.sanghavi001@gmail.com>
2020-08-21 17:56:20 +0530

    Using Firebase Utils Sign In With Email and Password Synced

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -6,6 +6,7 @@ import CustomButton from '../custom-button/custom-button.component';
 import {signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
+import { auth } from 'firebase';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -18,11 +19,18 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit=event=>{
+    handleSubmit=async event=>{
         event.preventDefault();
 
-        this.setState({email:'',password:''})
-    }
+        const{email,password}=this.state;
+
+        try{
+            auth().signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:''})
+        }catch(error){
+            console.log(error);
+        }
+    };
 
     handleChange=event=>{
         const {value,name}=event.target;
