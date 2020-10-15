756f0d709b7b8d7fe9838f27b718b144b2317fc3
yash001dev <y.sanghavi001@gmail.com>
2020-10-15 11:20:01 +0530

    Cart With Saga Added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,12 +10,12 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
-
+import {signOutStart} from '../../redux/user/user.actions';
 import { ReactComponent as Logo } from '../../assets/up.svg';
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden,signOutStart }) => (
     <HeaderContainer>
         <LogoContainer to='/'>
             <Logo className='logo' />
@@ -27,7 +27,7 @@ const Header = ({ currentUser, hidden }) => (
             <OptionLink to='/shop'>
                 CONTACT
             </OptionLink>
-            {currentUser ? (<OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>) : <OptionLink to='/signin'>SIGN IN</OptionLink>}
+            {currentUser ? (<OptionLink as='div' onClick={signOutStart}>SIGN OUT</OptionLink>) : <OptionLink to='/signin'>SIGN IN</OptionLink>}
             <CartIcon />
         </OptionsContainer>
         {
@@ -41,4 +41,8 @@ const mapStateToProps = createStructuredSelector({
     hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps=dispatch=>({
+    signOutStart:()=>dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(Header);
