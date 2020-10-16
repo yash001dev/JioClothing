169e7ce629e5c332fccf4c5a84b2b6630e06633f
yash001dev <y.sanghavi001@gmail.com>
2020-10-16 11:59:26 +0530

    SIGNUP WITH REDUX SAGA COMPLETED

--- src/components/sign-up/sign-up.components.jsx
+++ src/components/sign-up/sign-up.components.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {auth,createUserProfileDocument} from '../../firebase/firebase.utils';
+import {signUpStart} from '../../redux/user/user.actions';
+
+// import {auth,createUserProfileDocument} from '../../firebase/firebase.utils';
 
 import './sign-up.styles.scss';
 
@@ -21,7 +23,7 @@ class SignUp extends React.Component{
     handleSubmit=async event=>{
         
         event.preventDefault();
-
+        const {signUpStart}=this.props;
         const{displayName,email,password,confirmPassword}=this.state;
 
         if(password !== confirmPassword){
@@ -29,20 +31,8 @@ class SignUp extends React.Component{
             return;
         }
 
-        try {
-            const {user}=await auth.createUserWithEmailAndPassword(email,password);
-            await createUserProfileDocument(user,{displayName});
-
-            this.setState({
-                displayName:'',
-                email:'',
-                password:'',
-                confirmPassword:'',
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }
+        signUpStart({displayName,email,password});
+    };
 
     handleChange=event=>{
         const {name,value}=event.target;
@@ -101,6 +91,11 @@ class SignUp extends React.Component{
     }
 }
 
-export default SignUp;
+
+const mapDispatchToProps=dispatch=>({
+    signUpStart:userCredentials=>dispatch(signUpStart(userCredentials))
+})
+
+export default connect(null,mapDispatchToProps)(SignUp);
 
 
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,6 +5,8 @@ import {
   signInFailure,
   signOutSuccess,
   signOutFailure,
+  signUpSuccess,
+  signUpFailure,
 } from "./user.actions";
 import {
   auth,
@@ -13,9 +15,9 @@ import {
   getCurrentUser,
 } from "../../firebase/firebase.utils";
 
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth,additionalData) {
   try {
-    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userRef = yield call(createUserProfileDocument, userAuth,additionalData);
     const userSnapshot = yield userRef.get();
     yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
   } catch (error) {
@@ -60,6 +62,19 @@ export function* signOut() {
   }
 }
 
+export function* signUp({payload:{email,password,displayName}}){
+  try {
+    const {user}=yield auth.createUserWithEmailAndPassword(email,password);
+    yield put(signUpSuccess({user,additionalData:{displayName}}));
+  } catch (error) {
+    yield put(signUpFailure(error));
+  }
+}
+
+export function* signInAfterSignUp({payload:{user,additionalData}}){
+  yield getSnapshotFromUserAuth(user,additionalData);
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -76,11 +91,21 @@ export function* onSignOutStart() {
   yield takeLatest(UserActionTypes.SIGN_OUT_START,signOut);
 }
 
+export function* onSignUpStart(){
+  yield takeLatest(UserActionTypes.SIGN_UP_START,signUp)
+}
+
+export function* onSignUpSuccess(){
+  yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS,signInAfterSignUp)
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
     call(isUserAuthenticated),
     call(onSignOutStart),
+    call(onSignUpStart),
+    call(onSignUpSuccess),
   ]);
 }
